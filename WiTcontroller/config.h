60c51e47f9d58d03301512242366a194d5add39e
Peter Akers <akersp62@gmail.com>
2022-04-28 20:31:34 +1000

    Update config.h

--- WiTcontroller/config.h
+++ WiTcontroller/config.h
@@ -5,18 +5,18 @@ const char* deviceName = "WiT Controller";
 // enter the SSIDs and passwords of up to three networks you wish to try to connect to.
 // It will try for 10 seconds to connect before moving to the next.
 // leave the values blank/empty if you dont all three.  ssid0 must not be blank
-const char* ssid0     = "RMCQ";
-const char* password0 = "...";
-const char* ssid1     = "pra";
-const char* password1 = "...";
-const char* ssid2     = "RMCQnscaleEXHIBITION";
-const char* password2 = "...";
+const char* ssid0     = "MyWifi1";
+const char* password0 = "mywifi1 pasword";
+const char* ssid1     = "MyWifi2";
+const char* password1 = "MyWifi2 password";
+const char* ssid2     = "MyWifi3";
+const char* password2 = "MyWifi3 password";
 
-const String turnoutPrefixes[3] = {"...", "NT", "NT"};  // required if you wish to use turnouts  
-// this the prefix of all turnout system names, for three wiThrottle servers on the three networks above
+const String turnoutPrefixes[3] = {"NT", "NT", "NT"};  // required if you wish to use turnouts  
+// this the prefix of all turnout system names for YOUR, for three wiThrottle servers on the three networks above
 
-const String routePrefixes[3] = {"...", "IO:AUTO:", "IO:AUTO:"};  // required if you wish to use turnouts  
-// this the prefix of all turnout system names, for three wiThrottle servers on the three networks above
+const String routePrefixes[3] = {"IO:AUTO", "IO:AUTO:", "IO:AUTO:"};  // required if you wish to use routes  
+// this is the prefix of all route system names for YOUR system, for three wiThrottle servers on the three networks above
 
 // configure the keypad buttons to perform the actions you wish
 // 4x4 keypad
