7288475924133190c42560e6aaec3c095bdbd507
Peter Akers <akersp62@gmail.com>
2022-06-11 21:38:15 +1000

    Update config_network.h

--- config_network.h
+++ config_network.h
@@ -2,5 +2,7 @@
 // It will try for 10 seconds to connect before moving to the next.  
 // It will cycle back to the begining if all fail
 const int maxSsids = 3;
-const String ssids[maxSsids] = {"Network1", "Network2", "Network3"};
-const String passwords[maxSsids] = {"password1", "password2", "password3"};
+// const String ssids[maxSsids] = {"Network1", "Network2", "Network3"};
+// const String passwords[maxSsids] = {"password1", "password2", "password3"};
+const String ssids[maxSsids] = {"pra", "RMCQ", "RMCQnscaleEXHIBITION"};
+const String passwords[maxSsids] = {"startiderising", "", "buckleypark"};
