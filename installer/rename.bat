303be28789faf8d5f5588f28dce647f6022811f5
Tetsuo＠HMX <110798387+HMX-1972@users.noreply.github.com>
2024-07-06 19:34:03 +0900

    HMX02

--- installer/rename.bat
+++ installer/rename.bat
@@ -1,5 +1,5 @@
 del *.elf
 del *.map
-rename *.ino.bin P008.ino.bin
-rename *.ino.partitions.bin P008.ino.partitions.bin
-rename *.ino.bootloader.bin P008.ino.bootloader.bin
+rename *.ino.bin DCCEX.ino.bin
+rename *.ino.partitions.bin DCCEX.ino.partitions.bin
+rename *.ino.bootloader.bin DCCEX.ino.bootloader.bin
